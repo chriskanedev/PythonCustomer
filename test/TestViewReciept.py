f7a3cc9412160c90966811947f5786b7e0879000
Margonite1 <80271893+Margonite1@users.noreply.github.com>
2021-03-30 16:33:49 +0100

    some more refactoring

--- test/TestViewReciept.py
+++ test/TestViewReciept.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.Engine.view_receipt import Create_Receipt
+from src.Engine.view_receipt import create_receipt
 
 
 class MyTestCase(unittest.TestCase):
@@ -8,19 +8,19 @@ class MyTestCase(unittest.TestCase):
 
         menu = {"tea": 2.40, "coffee": 2.50}
         quantity = {"tea": 2, "coffee": 1}
-        result = Create_Receipt(menu, quantity)
+        result = create_receipt(menu, quantity)
         self.assertEqual(result, ['Order Number:TEST00', 2, 'tea', '£2.4', 1, 'coffee', '£2.5', 7.3])
 
     def testCreateReceipt2(self):
         menu = {"tea": 2.40, "coffee": 2.50, "green tea": 2.60}
         quantity = {"tea": 2, "coffee": 1, "green tea": 0}
-        result = Create_Receipt(menu, quantity)
+        result = create_receipt(menu, quantity)
         self.assertEqual(result, ['Order Number:TEST00', 2, 'tea', '£2.4', 1, 'coffee', '£2.5', 7.3])
 
     def testCreateReceipt3(self):
         menu = {"tea": 2.40, "coffee": 2.50, "Free Gift": 0.00}
         quantity = {"tea": 2, "coffee": 1, "Free Gift": 2}
-        result = Create_Receipt(menu, quantity)
+        result = create_receipt(menu, quantity)
         self.assertEqual(result, ['Order Number:TEST00', 2, 'tea', '£2.4', 1, 'coffee', '£2.5', 2,'Free Gift', '£0.0', 7.3])
 
 if __name__ == '__main__':
